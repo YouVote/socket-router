9e8147104638ef020fe443b093c1184269e2bfe1
Gabriel Wu <gabrielwu84@gmail.com>
2018-05-28 00:00:07 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+define({
+	socketScriptURL:"https://socketio-server-youvote.a3c1.starter-us-west-1.openshiftapps.com/socket.io/socket.io",
+	socketServerURL:"https://socketio-server-youvote.a3c1.starter-us-west-1.openshiftapps.com/",
+	// socketScriptURL:"http://socketio-server-youvote.a3c1.starter-us-west-1.openshiftapps.com/socket.io/socket.io",
+	// socketServerURL:"http://socketio-server-youvote.a3c1.starter-us-west-1.openshiftapps.com/",
+
+})
